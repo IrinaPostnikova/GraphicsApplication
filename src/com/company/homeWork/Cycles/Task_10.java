78f3eeb1e22f8b1bd0d949ff840a38564c62af13
IrinaPostnikova <iri.postnikova@gmail.com>
2022-04-14 23:55:01 +0300

    циклы

--- src/com/company/homeWork/Cycles/Task_10.java
+++ src/com/company/homeWork/Cycles/Task_10.java
@@ -1,9 +1,20 @@
 package com.company.homeWork.Cycles;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 //Задача_10/Пользователь вводит 1 число. Найти число, которое является зеркальным отображением последовательности
 // цифр заданного числа, например, задано число 123, вывести 321.
 public class Task_10 {
     public static void main(String[] args) {
-
+        Scanner s=new Scanner(System.in);
+        int a = s.nextInt();
+        int b= 0;
+        while (a!=0) {
+           b+=a%10;
+           b*=10;
+           a/=10;
+        }
+        System.out.println(b/10);
     }
 }
--- src/com/company/homeWork/Cycles/Task_19.java
+++ src/com/company/homeWork/Cycles/Task_19.java
@@ -1,53 +1,30 @@
 package com.company.homeWork.Cycles;
 
+
+import java.util.Scanner;
+
+//Можно ли заданное натуральное число М представить в виде суммы квадратов двух натуральных чисел?
+// Написать программу решения этой задачи.
 public class Task_19 {
     public static void main(String[] args) {
-
-        //Пользователь вводит 2 числа (A и B). Вывести сумму всех чисел из диапазона от A до B,
-        // которые делятся без остатка на 7. (Учтите, что при вводе B может оказаться меньше A).
-
-    /*  public static void task5() {
-          int a, b;
-          Scanner s = new Scanner(System.in);
-          a = s.nextInt();
-          b = s.nextInt();
-          int sum =0;
-          if (a > b) {
-              int c = a;
-              b = c;
-
-              for (int i = a; i <= b; i++) {
-                  if (i % 7 ==0)
-                      sum += i;
-                  }
-              }System.out.println("сумма=  " + sum);
-          }*/
- /* public static void task8(){
-
-  }*/
-/*//Можно ли заданное натуральное число М представить в виде суммы квадратов двух натуральных чисел?
-// Написать программу решения этой задачи.
-    public static void task19() {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        boolean flag = false;
+       // boolean flag = false;
         for (int i = 1; i <= Math.sqrt(a); i++) {
             for (int j = i; j <= Math.sqrt(a); j++) {
                 if (i * i + j * j == a) {
                     System.out.println("Можно");
-                    flag = true;
-                    break;
+                  //  flag = true;
+                  //  break;
+                    return;
                 }
-
             }
-            if (flag) break;
-
-
+           // if (flag) break;
         }
-        if (!flag)
+       // if (!flag)
             System.out.println("нельзя");
 
-    }*/
+
 
 
  /*   //public static void task19_2way()
