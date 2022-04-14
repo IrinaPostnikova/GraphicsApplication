66cce7e2c1c42a4968699b41977857e17e62b9b7
IrinaPostnikova <iri.postnikova@gmail.com>
2022-04-15 00:24:38 +0300

    масс

--- src/com/company/homeWork/Cycles/Task_11.java
+++ src/com/company/homeWork/Cycles/Task_11.java
@@ -1,8 +1,16 @@
 package com.company.homeWork.Cycles;
 
+import java.util.Scanner;
+
 //Задача_11/Пользователь вводит целое положительное  число (N). Выведите числа в диапазоне от 1 до N, сумма четных цифр которых больше суммы нечетных.
 public class Task_11 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите число N");
+        int N = s.nextInt();
+        for (int i = 1; i <=N; i++) {
 
+
+        }
     }
 }
